dd450fe30ad941dcba3f89a4d82c544172056f43
bohdanh <bohdanhu@softwareplanet.uk.com>
2017-09-18 11:09:23 +0300

    Add round and roundtype entities

--- src/main/java/com/incamp/mhs/round/Round.java
+++ src/main/java/com/incamp/mhs/round/Round.java
@@ -1,7 +1,11 @@
 package com.incamp.mhs.round;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.incamp.mhs.game.Game;
+import com.incamp.mhs.round.type.RoundType;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Lazy;
 
 import javax.persistence.*;
 
@@ -15,4 +19,25 @@ public class Round {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonView(MinimalView.class)
+    private Integer index;
+
+    @JsonView(MinimalView.class)
+    private String name;
+
+    @JsonView(MinimalView.class)
+    private Integer numberOfQuizzes;
+
+    @JsonView(MinimalView.class)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private RoundType roundType;
+
+    @JsonView(GameView.class)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Game game;
+
+    public interface MinimalView {}
+
+    public interface GameView extends MinimalView {}
 }
